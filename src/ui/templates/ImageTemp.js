05e1b6727ca2750b08c3bf2cacd234846d2f7095
yopiangga <yopigambyok@gmail.com>
2022-12-16 14:40:21 +0700

    Template Component Image & Text

--- src/ui/templates/ImageTemp.js
+++ src/ui/templates/ImageTemp.js
@@ -1,15 +1,42 @@
-export function ImageLogoTemp() {
-  return <div></div>;
+import logo from "src/assets/images/logo.png";
+import example from "src/assets/images/example.jpg";
+
+export function ImageLogoTemp({ item }) {
+  return (
+    <div
+      className={`absolute left-[${item.left}] top-[${item.top}] right-[${item.right}] bottom-[${item.bottom}] w-[${item.width}px] h-[${item.height}px] bg-[#${item.color}]`}
+    >
+      <img src={item.src == "" ? logo : item.src} alt="image" />
+    </div>
+  );
 }
 
-export function ImageSquareTemp() {
-  return <div></div>;
+export function ImageSquareTemp({ item }) {
+  return (
+    <div
+      className={`absolute left-[${item.left}] top-[${item.top}] right-[${item.right}] bottom-[${item.bottom}] w-[${item.width}px] h-[${item.height}px] bg-[#${item.color}] border border-[#${item.borderColor}] border-[${item.borderWidth}px]`}
+    >
+      <img src={item.src == "" ? example : item.src} alt="image" />
+    </div>
+  );
 }
 
-export function ImageSquareRoundedTemp() {
-  return <div></div>;
+export function ImageSquareRoundedTemp({ item }) {
+  return (
+    <div
+      className={`absolute left-[${item.left}] top-[${item.top}] right-[${item.right}] bottom-[${item.bottom}] w-[${item.width}px] h-[${item.height}px] bg-[#${item.color}] rounded-[${item.rounded}px] border border-[#${item.borderColor}] border-[${item.borderWidth}px]`}
+    >
+      <img src={item.src == "" ? example : item.src} alt="image" />
+    </div>
+  );
 }
 
-export function ImageCircleTemp() {
-  return <div></div>;
+export function ImageCircleTemp({ item }) {
+  return (
+    <div
+      className={`absolute left-[${item.left}] top-[${item.top}] right-[${item.right}] bottom-[${item.bottom}] w-[${item.width}px] h-[${item.height}px] bg-[#${item.color}] rounded-full border border-[#${item.borderColor}] border-[${item.borderWidth}px]`}
+    >
+      <img src={item.src == "" ? example : item.src} alt="image" />
+    </div>
+  );
 }
--- src/ui/templates/TextTemp.js
+++ src/ui/templates/TextTemp.js
@@ -1,3 +1,13 @@
-export function TextTemp() {
-  return <div></div>;
+export function TextTemp({ item }) {
+  return (
+    <div
+      className={`absolute left-[${item.left}] top-[${item.top}] right-[${item.right}] bottom-[${item.bottom}] w-[${item.width}px] h-[${item.height}px] text-[#${item.color}] `}
+    >
+      <p
+        className={`font-[${item.fontSize}px] text-${item.textAlign} font-${item.fontWeight}`}
+      >
+        {item.text}
+      </p>
+    </div>
+  );
 }
