42e56b3e52ec4503f07a028eac66c2aefd0dffec
yopiangga <yopigambyok@gmail.com>
2022-12-16 16:45:26 +0700

    Scalling frame

--- src/ui/templates/FrameTemp.js
+++ src/ui/templates/FrameTemp.js
@@ -1,9 +1,15 @@
 import SVG from "react-inlinesvg";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { RatioTemplateContext } from "src/context/RatioTemplateContext";
 
 export function FrameTemp({ item }) {
   const { ratioTemplate, setRatioTemplate } = useContext(RatioTemplateContext);
+  const [width, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [newSvg, setNewSvg] = useState("");
+
+  const svg = item.svg;
+
   return (
     <div
       className={`absolute left-[${item.left * ratioTemplate}px] top-[${
@@ -12,7 +18,9 @@ export function FrameTemp({ item }) {
         item.bottom * ratioTemplate
       }px] w-[${item.width * ratioTemplate}px] h-[${
         item.height * ratioTemplate
-      }px]`}
+      }px] scale-[${ratioTemplate * 100}%] translate-y-[${
+        (100 - ratioTemplate * 100) / 2
+      }%] -translate-x-[${(100 - ratioTemplate * 100) / 2}%]`}
     >
       <SVG src={item.svg} />
     </div>
