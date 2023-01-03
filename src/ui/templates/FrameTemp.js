6efcb3e365a5f36daa810567115dfe967e4786e4
yopiangga <yopigambyok@gmail.com>
2023-01-03 22:33:54 +0700

    Fix: Ratio Frame

--- src/ui/templates/FrameTemp.js
+++ src/ui/templates/FrameTemp.js
@@ -5,13 +5,6 @@ import { RatioTemplateContext } from "src/context/RatioTemplateContext";
 export function FrameTemp({ item, colors }) {
   const { ratioTemplate, setRatioTemplate } = useContext(RatioTemplateContext);
 
-  let vertical = "";
-  if (item.top === null || item.top === undefined) {
-    vertical = `bottom-[${item.bottom * ratioTemplate}px]`;
-  } else {
-    vertical = `top-[${item.top * ratioTemplate}px]`;
-  }
-
   return (
     <div
       className={`absolute left-[${item.left * ratioTemplate}px] right-[${
