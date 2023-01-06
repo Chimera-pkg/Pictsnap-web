e64f22f5c0efc7c9c99ecf6540fcfc897e50df73
= <=>
2023-01-07 00:13:18 +0700

    feat: delete prevention

--- src/ui/components/ModalInformationComponent.js
+++ src/ui/components/ModalInformationComponent.js
@@ -50,7 +50,7 @@ export function ModalConfirmationComponent(props) {
       >
         <div className="relative max-w-2xl md:h-auto">
           {/* Modal content */}
-          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-800">
             {/* Modal header */}
             <div className="flex justify-between items-start p-4 rounded-t border-b dark:border-gray-600">
               <h3 className="text-xl font-semibold text-gray-900 dark:text-white">
@@ -70,15 +70,13 @@ export function ModalConfirmationComponent(props) {
             {/* Modal footer */}
             <div className="flex items-center p-6 space-x-2 rounded-b border-t border-gray-200 dark:border-gray-600">
               <button
-                data-modal-toggle="defaultModal"
                 type="button"
                 onClick={props.handleSubmit}
-                className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                className="flex-no-shrink bg-[#FB8500] hover:bg-[#FFB703] px-5 ml-4 py-2 text-sm shadow-sm hover:shadow-lg font-medium tracking-wider border-2 border-[#FB8500] text-white rounded-lg"
               >
                 Kirim
               </button>
               <button
-                data-modal-toggle="defaultModal"
                 type="button"
                 onClick={props.handleClose}
                 className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
